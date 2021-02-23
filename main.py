b399a87eb7a2eaec6e9effcce0c8530bfb6fa6c2
Boris <boris2000n@gmail.com>
2021-02-23 14:15:59 +0300

    auto

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ if __name__ == '__main__':
     limit = 3
     runner = SimpleInfiniteRunner()
     runner.add(start_pooling)
-    runner.add(run_spider(LinksSpider), limit=limit, sleep=3600)
-    runner.add(run_spider(ProductPageSpider), sleep=30)
+    # runner.add(run_spider(LinksSpider), limit=limit, sleep=3600)
+    # runner.add(run_spider(ProductPageSpider), sleep=30)
     runner.add(run_updater(TranslateUpdater(limit=limit)), sleep=10)
-    runner.add(run_updater(CheckAvailableRowsUpdater(limit=limit)), sleep=30)
+    # runner.add(run_updater(CheckAvailableRowsUpdater(limit=limit)), sleep=30)
     runner.start()
